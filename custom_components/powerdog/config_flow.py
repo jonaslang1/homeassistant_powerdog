ac353adaba9149901524c5b07f76fc4d21f00dd1
Jonas Lang <jonaslang@jonaslang.de>
2022-12-04 19:26:49 +0000

    added strings.json

--- custom_components/powerdog/config_flow.py
+++ custom_components/powerdog/config_flow.py
@@ -8,7 +8,7 @@ import voluptuous as vol
 
 from homeassistant import config_entries
 from homeassistant.const import CONF_EMAIL, CONF_PASSWORD
-from homeassistant.data_entry_flow import FlowResult, FlowHandler
+from homeassistant.data_entry_flow import FlowResult
 import homeassistant.helpers.config_validation as cv
 
 from powerdog import powerdog_api
@@ -17,8 +17,8 @@ from .const import DOMAIN
 _LOGGER = logging.getLogger(__name__)
 
 
-#@config_entries.HANDLERS.register(DOMAIN)
-class PowerdogConfigFlow(FlowHandler, domain=DOMAIN):
+@config_entries.HANDLERS.register(DOMAIN)
+class PowerdogConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     """Handle a config flow for PowerDog."""
 
     VERSION = 1
@@ -27,42 +27,33 @@ class PowerdogConfigFlow(FlowHandler, domain=DOMAIN):
         self, user_input = None
     ) -> FlowResult:
         """User step in config flow."""
-        data_schema = {
-            vol.Required("username"): str,
-            vol.Required("password"): str,
-        }
         errors = {}
+        if user_input is not None:
+            try:
+                apikey = powerdog_api.get_apikey(
+                    user_input[CONF_EMAIL], user_input[CONF_PASSWORD]
+                )
+            except powerdog_api.APICallResultInvalidException:
+                errors["base"] = "cannot_connect"
+            except powerdog_api.NoItemFoundException:
+                errors["base"] = "cannot_retrieve_device_info"
+
+            if not errors:
+                await self.async_set_unique_id(
+                    powerdog_api.get_powerdog_uids(apikey)[0]
+                )
+                self._abort_if_unique_id_configured()
+            return self.async_create_entry(
+                title=user_input[CONF_EMAIL], data=user_input
+            )
 
         return self.async_show_form(
-            step_id="init", data_schema=vol.Schema(data_schema), errors=errors
+            step_id="user",
+            data_schema=vol.Schema(
+                {
+                    vol.Required(CONF_EMAIL): cv.string,
+                    vol.Required(CONF_PASSWORD): cv.string,
+                }
+            ),
+            errors=errors,
         )
-        # errors = {}
-        # if user_input is not None:
-        #     try:
-        #         apikey = powerdog_api.get_apikey(
-        #             user_input[CONF_EMAIL], user_input[CONF_PASSWORD]
-        #         )
-        #     except powerdog_api.APICallResultInvalidException:
-        #         errors["base"] = "cannot_connect"
-        #     except powerdog_api.NoItemFoundException:
-        #         errors["base"] = "cannot_retrieve_device_info"
-
-        #     if not errors:
-        #         await self.async_set_unique_id(
-        #             powerdog_api.get_powerdog_uids(apikey)[0]
-        #         )
-        #         self._abort_if_unique_id_configured()
-        #     return self.async_create_entry(
-        #         title=user_input[CONF_EMAIL], data=user_input
-        #     )
-
-        # return self.async_show_form(
-        #     step_id="user",
-        #     data_schema=vol.Schema(
-        #         {
-        #             vol.Required(CONF_EMAIL): cv.string,
-        #             vol.Required(CONF_PASSWORD): cv.string,
-        #         }
-        #     ),
-        #     errors=errors,
-        # )
