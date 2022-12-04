41a8ccb349281549bfac81ef429b24dc8b59e483
Jonas Lang <jonaslang@jonaslang.de>
2022-12-04 19:37:32 +0000

    flow

--- custom_components/powerdog/config_flow.py
+++ custom_components/powerdog/config_flow.py
@@ -27,24 +27,24 @@ class PowerdogConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     ) -> FlowResult:
         """User step in config flow."""
         errors = {}
-        # if user_input is not None:
-        #     try:
-        #         apikey = powerdog_api.get_apikey(
-        #             user_input[CONF_EMAIL], user_input[CONF_PASSWORD]
-        #         )
-        #     except powerdog_api.APICallResultInvalidException:
-        #         errors["base"] = "cannot_connect"
-        #     except powerdog_api.NoItemFoundException:
-        #         errors["base"] = "cannot_retrieve_device_info"
+        if user_input is not None:
+            try:
+                apikey = powerdog_api.get_apikey(
+                    user_input[CONF_EMAIL], user_input[CONF_PASSWORD]
+                )
+            except powerdog_api.APICallResultInvalidException:
+                errors["base"] = "cannot_connect"
+            except powerdog_api.NoItemFoundException:
+                errors["base"] = "cannot_retrieve_device_info"
 
-        #     if not errors:
-        #         await self.async_set_unique_id(
-        #             powerdog_api.get_powerdog_uids(apikey)[0]
-        #         )
-        #         self._abort_if_unique_id_configured()
-        #     return self.async_create_entry(
-        #         title=user_input[CONF_EMAIL], data=user_input
-        #     )
+            if not errors:
+                await self.async_set_unique_id(
+                    powerdog_api.get_powerdog_uids(apikey)[0]
+                )
+                self._abort_if_unique_id_configured()
+            return self.async_create_entry(
+                title=user_input[CONF_EMAIL], data=user_input
+            )
 
         return self.async_show_form(
             step_id="user",
