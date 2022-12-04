d47de82ddd454cc42dd65528807f67dbe89b1fde
Jonas Lang <jonaslang@jonaslang.de>
2022-12-04 19:30:10 +0000

    Flowhandler

--- custom_components/powerdog/config_flow.py
+++ custom_components/powerdog/config_flow.py
@@ -8,7 +8,7 @@ import voluptuous as vol
 
 from homeassistant import config_entries
 from homeassistant.const import CONF_EMAIL, CONF_PASSWORD
-from homeassistant.data_entry_flow import FlowResult
+from homeassistant.data_entry_flow import FlowResult, FlowHandler
 import homeassistant.helpers.config_validation as cv
 
 from powerdog import powerdog_api
@@ -18,7 +18,7 @@ _LOGGER = logging.getLogger(__name__)
 
 
 @config_entries.HANDLERS.register(DOMAIN)
-class PowerdogConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+class PowerdogConfigFlow(FlowHandler, domain=DOMAIN):
     """Handle a config flow for PowerDog."""
 
     VERSION = 1
@@ -28,24 +28,24 @@ class PowerdogConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     ) -> FlowResult:
         """User step in config flow."""
         errors = {}
-        if user_input is not None:
-            try:
-                apikey = powerdog_api.get_apikey(
-                    user_input[CONF_EMAIL], user_input[CONF_PASSWORD]
-                )
-            except powerdog_api.APICallResultInvalidException:
-                errors["base"] = "cannot_connect"
-            except powerdog_api.NoItemFoundException:
-                errors["base"] = "cannot_retrieve_device_info"
+        # if user_input is not None:
+        #     try:
+        #         apikey = powerdog_api.get_apikey(
+        #             user_input[CONF_EMAIL], user_input[CONF_PASSWORD]
+        #         )
+        #     except powerdog_api.APICallResultInvalidException:
+        #         errors["base"] = "cannot_connect"
+        #     except powerdog_api.NoItemFoundException:
+        #         errors["base"] = "cannot_retrieve_device_info"
 
-            if not errors:
-                await self.async_set_unique_id(
-                    powerdog_api.get_powerdog_uids(apikey)[0]
-                )
-                self._abort_if_unique_id_configured()
-            return self.async_create_entry(
-                title=user_input[CONF_EMAIL], data=user_input
-            )
+        #     if not errors:
+        #         await self.async_set_unique_id(
+        #             powerdog_api.get_powerdog_uids(apikey)[0]
+        #         )
+        #         self._abort_if_unique_id_configured()
+        #     return self.async_create_entry(
+        #         title=user_input[CONF_EMAIL], data=user_input
+        #     )
 
         return self.async_show_form(
             step_id="user",
