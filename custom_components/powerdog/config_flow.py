24dcf17690f2e2964235eed566c13f8f3d60d741
Jonas Lang <jonaslang@jonaslang.de>
2022-12-04 19:03:11 +0000

    flow test

--- custom_components/powerdog/config_flow.py
+++ custom_components/powerdog/config_flow.py
@@ -23,44 +23,39 @@ class PowerdogConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 
     VERSION = 1
 
-    def __init__(self) -> None:
-        """Initialize."""
-        self._data = {
-            CONF_EMAIL: vol.UNDEFINED,
-            CONF_PASSWORD: vol.UNDEFINED,
-        }
-
     async def async_step_user(
-        self, user_input: dict[str, Any] | None = None
+        self, user_input = None
     ) -> FlowResult:
-        """First step in config flow."""
-        errors = {}
-        if user_input is not None:
-            try:
-                apikey = powerdog_api.get_apikey(
-                    user_input[CONF_EMAIL], user_input[CONF_PASSWORD]
-                )
-            except powerdog_api.APICallResultInvalidException:
-                errors["base"] = "cannot_connect"
-            except powerdog_api.NoItemFoundException:
-                errors["base"] = "cannot_retrieve_device_info"
+        """User step in config flow."""
+        #return self.async_show_form(None)
 
-            if not errors:
-                await self.async_set_unique_id(
-                    powerdog_api.get_powerdog_uids(apikey)[0]
-                )
-                self._abort_if_unique_id_configured(updates=self._data)
-            return self.async_create_entry(
-                title=self._data[CONF_EMAIL], data=self._data
-            )
+        # errors = {}
+        # if user_input is not None:
+        #     try:
+        #         apikey = powerdog_api.get_apikey(
+        #             user_input[CONF_EMAIL], user_input[CONF_PASSWORD]
+        #         )
+        #     except powerdog_api.APICallResultInvalidException:
+        #         errors["base"] = "cannot_connect"
+        #     except powerdog_api.NoItemFoundException:
+        #         errors["base"] = "cannot_retrieve_device_info"
 
-        return self.async_show_form(
-            step_id="user",
-            data_schema=vol.Schema(
-                {
-                    vol.Required(CONF_EMAIL): cv.string,
-                    vol.Required(CONF_PASSWORD): cv.string,
-                }
-            ),
-            errors=errors,
-        )
+        #     if not errors:
+        #         await self.async_set_unique_id(
+        #             powerdog_api.get_powerdog_uids(apikey)[0]
+        #         )
+        #         self._abort_if_unique_id_configured()
+        #     return self.async_create_entry(
+        #         title=user_input[CONF_EMAIL], data=user_input
+        #     )
+
+        # return self.async_show_form(
+        #     step_id="user",
+        #     data_schema=vol.Schema(
+        #         {
+        #             vol.Required(CONF_EMAIL): cv.string,
+        #             vol.Required(CONF_PASSWORD): cv.string,
+        #         }
+        #     ),
+        #     errors=errors,
+        # )
